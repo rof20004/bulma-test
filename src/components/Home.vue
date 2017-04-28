dcfe7731b01ef86a2e3666e1264f82f00b7ec075
rof20004 <rof20004@gmail.com>
2017-04-28 13:56:42 -0400

    Testing nav, menus and responsiveness

---
+++ src/components/Home.vue
@@ -0,0 +1,136 @@
+<template>
+  <div class="login">
+    <nav class="nav has-shadow">
+      <div class="nav-left">
+        <!-- This "nav-toggle" hamburger menu is only visible on mobile -->
+        <!-- You need JavaScript to toggle the "is-active" class on "nav-menu" -->
+        <span class="nav-toggle">
+          <span></span>
+          <span></span>
+          <span></span>
+        </span>
+        <a class="nav-item">
+          <img src="http://bulma.io/images/bulma-logo.png" alt="Bulma logo">
+        </a>
+      </div>
+
+      <div class="nav-center">
+        <a class="nav-item">
+          <span class="icon">
+            <i class="fa fa-github"></i>
+          </span>
+        </a>
+        <a class="nav-item">
+          <span class="icon">
+            <i class="fa fa-twitter"></i>
+          </span>
+        </a>
+      </div>
+
+      <!-- This "nav-toggle" hamburger menu is only visible on mobile -->
+      <!-- You need JavaScript to toggle the "is-active" class on "nav-menu" -->
+      <span class="nav-toggle">
+        <span></span>
+        <span></span>
+        <span></span>
+      </span>
+
+      <!-- This "nav-menu" is hidden on mobile -->
+      <!-- Add the modifier "is-active" to display it on mobile -->
+      <div class="nav-right nav-menu">
+        <a class="nav-item">
+          Home
+        </a>
+        <a class="nav-item">
+          Documentation
+        </a>
+        <a class="nav-item">
+          Blog
+        </a>
+
+        <div class="nav-item">
+          <div class="field is-grouped">
+            <p class="control">
+              <a class="button" >
+                <span class="icon">
+                  <i class="fa fa-twitter"></i>
+                </span>
+                <span>Tweet</span>
+              </a>
+            </p>
+            <p class="control">
+              <a class="button is-primary">
+                <span class="icon">
+                  <i class="fa fa-download"></i>
+                </span>
+                <span>Download</span>
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <span class="nav-toogle">
+      
+    </span>
+    <aside class="menu is-hidden-mobile">
+      <p class="menu-label">
+        General
+      </p>
+      <ul class="menu-list">
+        <li><a>Dashboard</a></li>
+        <li><a>Customers</a></li>
+      </ul>
+      <p class="menu-label">
+        Administration
+      </p>
+      <ul class="menu-list">
+        <li><a>Team Settings</a></li>
+        <li>
+          <a class="is-active">Manage Your Team</a>
+          <ul>
+            <li><a>Members</a></li>
+            <li><a>Plugins</a></li>
+            <li><a>Add a member</a></li>
+          </ul>
+        </li>
+        <li><a>Invitations</a></li>
+        <li><a>Cloud Storage Environment Settings</a></li>
+        <li><a>Authentication</a></li>
+      </ul>
+      <p class="menu-label">
+        Transactions
+      </p>
+      <ul class="menu-list">
+        <li><a>Payments</a></li>
+        <li><a>Transfers</a></li>
+        <li><a>Balance</a></li>
+      </ul>
+    </aside>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data () {
+    return {
+      isLoading: false
+    }
+  },
+  methods: {
+    execute () {
+      console.log('Executou!')
+    }
+  }
+}
+</script>
+
+<style scoped>
+aside {
+  background-color: yellow;
+  position: absolute;
+  width: calc(100% - 980px);
+  height: calc(100% - 52px);
+}
+</style>
